adbd951170ce6817c649e5f5818193c14da64896
Chris Van Huffe <cvanhuffel@outlook.com>
2021-12-09 20:14:36 -0800

    Quiz app added

---
+++ quiz-app/script.js
@@ -0,0 +1,121 @@
+const quizData = [
+  {
+      question: "Which language runs in a web browser?",
+      a: "Java",
+      b: "C",
+      c: "Python",
+      d: "JavaScript",
+      correct: "d",
+  },
+  {
+      question: "What does CSS stand for?",
+      a: "Central Style Sheets",
+      b: "Cascading Style Sheets",
+      c: "Cascading Simple Sheets",
+      d: "Cats Style Sheets",
+      correct: "b",
+  },
+  {
+      question: "What does HTML stand for?",
+      a: "Hypertext Markup Language",
+      b: "Hypertext Markdown Language",
+      c: "Hyperloop Machine Language",
+      d: "Hyper Turtles Moonwalk Lasagna",
+      correct: "a",
+  },
+  {
+      question: "What year was JavaScript launched?",
+      a: "1896",
+      b: "1995",
+      c: "1994",
+      d: "none of the above",
+      correct: "b",
+  },
+  {
+      question: "What does API stand for?",
+      a: "Application Programming Interference",
+      b: "Appliance Programming Interface",
+      c: "Application Programming Interface",
+      d: "Animal Planet Investigator",
+      correct: "c",
+  },
+  {
+      question: "What programming language is named after a gemstone?",
+      a: "Sapphire",
+      b: "Ruby",
+      c: "Emerald",
+      d: "All of the above",
+      correct: "d",
+  },
+  {
+      question: "Who's cats are beautiful?",
+      a: "My cats",
+      b: "Your cats",
+      c: "The neighbor's cats ",
+      d: "All cats",
+      correct: "d",
+  },
+];
+
+const answerEls = document.querySelectorAll('.answer')
+const quiz = document.getElementById('quiz')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+  deselectAnswers()
+  const currentQuizData = quizData[currentQuiz]
+
+  questionEl.innerText = currentQuizData.question
+  a_text.innerText = currentQuizData.a 
+  b_text.innerText = currentQuizData.b 
+  c_text.innerText = currentQuizData.c 
+  d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+  answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+  let answer
+
+  answerEls.forEach(answerEl => {
+    if(answerEl.checked) {
+      answer = answerEl.id
+    }
+  })
+  return answer
+}
+
+submitBtn.addEventListener('click', () => {
+  const answer = getSelected()
+  
+  if(answer) {
+    if(answer === quizData[currentQuiz].correct) {
+      score++
+    }
+
+    currentQuiz++
+
+    if(currentQuiz < quizData.length) {
+      loadQuiz()
+    } else {
+      quiz.innerHTML = `
+        <h2>You answered ${score}/${quizData.length}
+        questions correctly.</h2>
+
+        <button onclick="location.reload()">Reload</button>
+      `
+    }
+  }
+})
